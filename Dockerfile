1dc6dd68b35a35278233f0f230bb4025036175c9
mophite <1743299@qq.com>
2025-09-18 18:35:14 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -5,26 +5,20 @@ ARG BUILD_ARCH
 
 ENV APP_PATH="/usr/src"
 
+# 复制压缩包到临时目录
+COPY hahub.tar.gz /tmp/
+
 # 根据架构选择正确的二进制文件
 RUN \
-    case "$BUILD_ARCH" in \
-        "aarch64") MACHINE="arm64" ;; \
-        "amd64")   MACHINE="amd64" ;; \
-        "armhf")   MACHINE="arm" ;; \
-        "armv7")   MACHINE="arm" ;; \
-        "i386")    MACHINE="386" ;; \
-        *) echo "Unsupported architecture: $BUILD_ARCH" && exit 1 ;; \
-    esac && \
-    echo "Architecture: $BUILD_ARCH, Machine: $MACHINE" && \
     FILE_NAME="hahub.tar.gz" && \
-    echo "Copying ${FILE_NAME} from project root" && \
-    mkdir -p ${APP_PATH} && \
-    # 解压缩并复制文件
     tar -xzf /tmp/${FILE_NAME} -C ${APP_PATH} && \
     echo "Copied ${FILE_NAME} to ${APP_PATH}"
 
 # 复制启动脚本
 COPY run.sh /
 RUN chmod a+x /run.sh
+
+# 确保可执行文件具有执行权限
+RUN chmod a+x ${APP_PATH}/hahub
 
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 set -e
 
 # 定义配置文件路径
-CONFIG_PATH=/data/hahub.toml
 LOG_FILE="/data/hahub.log"
 APP_PATH="/usr/src"
 WAIT_PIDS=()
@@ -13,16 +12,6 @@ function stop_hahub() {
     kill -15 "${WAIT_PIDS[@]}"
 }
 
-if [ -f "${CONFIG_PATH}" ]; then
-    bashio::log.info "Removing old configuration file..."
-    rm -f "${CONFIG_PATH}"
-fi
-
-# 显示欢迎信息
-ALL_CONFIG=$(bashio::config --all)
-bashio::log.info "Starting hahub Client...${ALL_CONFIG}"
-
-cat /data/options.json
 
 # 检查 hahub 是否存在
 if [ ! -f "${APP_PATH}/hahub" ]; then
@@ -40,28 +29,13 @@ bashio::log.info "Creating hahub Client/Server configuration..."
 bashio::log.info "Configuration created with following settings:"
 bashio::log.info "PHONE: ${PHONE}"
 
-# 创建 TOML 配置文件
-cat > "${CONFIG_PATH}" << EOL
-phone = "${PHONE}"
-EOL
-
-cat $CONFIG_PATH
-
-# 检查配置文件是否存在
-if [ ! -f "${CONFIG_PATH}" ]; then
-    bashio::log.error "Configuration file not found at ${CONFIG_PATH}"
-    exit 1
-fi
-
 export HASS_PHONE="${PHONE}"
 export HASS_SERVER="http://supervisor/core"
 export HASS_TOKEN="${SUPERVISOR_TOKEN:-}"
 
 # 启动 hahub
-bashio::log.info "Starting hahub Client/Server with configuration at ${CONFIG_PATH}"
 cd /usr/src
-./hahub -c $CONFIG_PATH > "${LOG_FILE}" 2>&1 & WAIT_PIDS+=($!) & tail -f ${LOG_FILE}
-
+./hahub > "${LOG_FILE}" 2>&1 & WAIT_PIDS+=($!) & tail -f ${LOG_FILE}
 
 
 trap "stop_hahub" SIGTERM SIGHUP
