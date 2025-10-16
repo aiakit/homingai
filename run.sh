e0e2f34381ca9a49143cc2602e17529a43c2e26b
mophite <1743299@qq.com>
2025-10-16 20:21:36 +0800

    update

--- run.sh
+++ run.sh
@@ -29,28 +29,33 @@ WAKE_UP_MAIL=$(bashio::config 'wake_up_mail')
 OPENAI_ADDRESS=$(bashio::config 'openai_address')
 OPENAI_KEY=$(bashio::config 'openai_key')
 OPENAI_MODE=$(bashio::config 'openai_mode')
+OPENAI_FAST_MODE=$(bashio::config 'openai_fast_mode')
 FILTER_MESSAGE=$(bashio::config 'filter_message')
 POETRY=$(bashio::config 'poetry')
 TOO_LONG=$(bashio::config 'too_long')
 IS_DISPLAY=$(bashio::config 'is_display')
+DISPLAY_AREA=$(bashio::config 'display_area')
 
 bashio::log.info "Creating hahub Client/Server configuration..."
 bashio::log.info "Configuration created with following settings:"
 bashio::log.info "MAIL: ${MAIL}"
 bashio::log.info "PHONE: ${PHONE}"
 bashio::log.info "OPENAI_MODE: ${OPENAI_MODE}"
+bashio::log.info "OPENAI_FAST_MODE: ${OPENAI_FAST_MODE}"
 bashio::log.info "OPENAI_ADDRESS: ${OPENAI_ADDRESS}"
 bashio::log.info "OPENAI_KEY: ${OPENAI_KEY}"
 bashio::log.info "FILTER_MESSAGE: ${FILTER_MESSAGE}"
-bashio::log.info "FILTER_MESSAGE: ${POETRY}"
-bashio::log.info "FILTER_MESSAGE: ${TOO_LONG}"
-bashio::log.info "FILTER_MESSAGE: ${IS_DISPLAY}"
-bashio::log.info "FILTER_MESSAGE: ${WAKE_UP_MAIL}"
+bashio::log.info "POETRY: ${POETRY}"
+bashio::log.info "TOO_LONG: ${TOO_LONG}"
+bashio::log.info "IS_DISPLAY: ${IS_DISPLAY}"
+bashio::log.info "WAKE_UP_MAIL: ${WAKE_UP_MAIL}"
+bashio::log.info "DISPLAY_AREA: ${DISPLAY_AREA}"
 
 export HASS_PHONE="${PHONE}"
 export HASS_MAIL="${MAIL}"
 export HASS_WAKE_UP_MAIL="${WAKE_UP_MAIL}"
 export HASS_OPENAI_MODE="${OPENAI_MODE}"
+export HASS_OPENAI_FAST_MODE="${OPENAI_FAST_MODE}"
 export HASS_OPENAI_ADDRESS="${OPENAI_ADDRESS}"
 export HASS_OPENAI_KEY="${OPENAI_KEY}"
 export HASS_SERVER="http://supervisor/core"
@@ -59,6 +64,7 @@ export HASS_FILTER_MESSAGE="${FILTER_MESSAGE}"
 export HASS_POETRY="${POETRY}"
 export HASS_TOO_LONG="${TOO_LONG}"
 export HASS_IS_DISPLAY="${IS_DISPLAY}"
+export HASS_DISPLAY_AREA="${DISPLAY_AREA}"
 
 # 启动 hahub
 cd /usr/src
